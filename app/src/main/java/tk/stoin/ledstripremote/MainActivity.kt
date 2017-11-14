5b7c0de7af867062ee84f3df1319b13ae049872a
Stijn van Gils <stijn-28@hotmail.com>
2017-11-14 23:28:35 +0100

    Initial Commit

---
+++ app/src/main/java/tk/stoin/ledstripremote/MainActivity.kt
@@ -0,0 +1,12 @@
+package tk.stoin.ledstripremote
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
